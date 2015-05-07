cb57cf2da03bbc00fd60a24baa65268cb2d39d2e
Michelle Valente <ma3360@columbia.edu>
2015-05-07 00:42:36 -0400

    LINKDOWN working

--- PA2/bfclient.py
+++ PA2/bfclient.py
@@ -5,22 +5,55 @@ import threading
 import time
 
 routingTable = {}
-neighborDV = {}
+neighborRT = {}
 distanceVector = {}
-neighbors = []
+neighbors = {}
 myAddr = []
 
-# def updateTable(new_table, sender):
-# 	for node in new_table:
-# 		nodeAddr, nodePort = node.split(':')
-# 		if(nodeAddr != myAddr[0] && nodePort != myAddr[1]):
-# 			if(node not in routingTable):
-# 				routingTable[node] = (float('inf'), "")
+class Message:
+	def __init__(self, message_type, message):
+		self.message_type = message_type
+		self.message = message
 
-# 			for old_node in routingTable:
-# 				cost = routingTable[old_node][0]
-# 				newCost = routingTable[sender][0] + new_table
+def printRoutingTable():
+	time.sleep(2)
+	print("TESTE")
+	for neighbor in neighborRT:
+		table = neighborRT[neighbor]
+		for node in table:
+			print("Node: "),
+			print(node)
+			print("Cost: "),
+			print(table[node][0])
+			print("Next hop: "),
+			print(table[node][1])
 
+def updateTable(new_table, sender):
+	''' Update routing tables '''
+	if( sender in neighborRT ):
+		dictSender = neighborRT[sender]
+		for node in new_table:
+			if(node not in dictSender ):
+				dictSender[node] = new_table[node]
+			else:
+				if(dictSender[node][0] != new_table[node][0] or dictSender[node][1] != new_table[node][1] ):
+					dictSender[node] = (new_table[node][0], new_table[node][1])
+	else:
+		neighborRT[sender] = new_table
+
+	for node in new_table:
+		myNode = str(myAddr[0]) + ":" + str(myAddr[1])
+		if(node != myNode):
+			if(node not in routingTable):
+				routingTable[node] = (routingTable[sender][0] + new_table[node][0], sender)
+
+			else:
+				cost = routingTable[node][0]
+				newCost = routingTable[sender][0] + new_table[node][0]
+				if(newCost == float('inf') and node not in neighbors):
+					routingTable[node] = (float('inf'), "")
+				elif(newCost < cost):
+					routingTable[node] = (newCost, sender)
 
 def ReadFile(config_file):
 	''' Reads configuration file '''
@@ -35,33 +68,68 @@ def ReadFile(config_file):
 		info = line.split()
 		neighbor = info[0]
 		neighbor_distance = float(info[1])
-		distanceVector[neighbor] = neighbor_distance
-		neighbors.append(neighbor)
+		neighbors[neighbor] = neighbor_distance
 
 		# Initialize routing table (tuple with distance and next hop)
-		routingTable[neighbor] = (neighbor_distance, "")
+		routingTable[neighbor] = (neighbor_distance, neighbor)
 
 	return (port, timeout)
 
 def sendRT(s):
-	''' Sends Routing table to neighbors '''
+	''' Sends distance vector to neighbors '''
+
 	for client in neighbors:
 		clientAddr, clientPort = client.split(':')
-		#s.sendto("update", (clientAddr,int(clientPort)))
-		s.sendto(pickle.dumps(routingTable), (clientAddr,int(clientPort)))
+		defineDistanceVector(client)
+		messageSnd = Message("update", distanceVector)
+		s.sendto(pickle.dumps(messageSnd), (clientAddr,int(clientPort)))
+
+def sendMessage(s, message_type, message, client):
+	clientAddr, clientPort = client.split(':')
+	messageSnd = Message(message_type, message)
+	s.sendto(pickle.dumps(messageSnd), (clientAddr,int(clientPort)))
+
 
 def executeTimeout(timeout, s):
+	''' Send routing table after every timeout'''
 	while(True):
 		time.sleep(timeout)
 		sendRT(s)
 
 def getRT(s):
+	''' get routing table from other nodes '''
 	while(True):
 		data, addr = s.recvfrom(1024)
-		new_rt = pickle.loads(data)
-		updateTable(new_rt)
 
-def getCommand():
+		sender = str(addr[0]) + ":" + str(addr[1])
+		messageRcv = pickle.loads(data)
+		if(messageRcv.message_type == "update"):
+			updateTable(messageRcv.message, sender)
+		if(messageRcv.message_type == "linkdown"):
+			print("linkdown")
+			routingTable[sender] = (float('inf'), "")
+
+def linkDown(s, Addr, Port):
+	''' execute command LINKDOWN ''' 
+	node = str(Addr) + ":" + str(Port)
+	if(node in neighbors):
+		routingTable[node] = (float('inf'), "")
+		sendMessage(s, "linkdown", "linkdown", node)
+		sendRT(s)
+	else:
+		print("Invalid parameters for LINKDOWN")
+
+def linkUp(s, Addr, Port):
+	node = str(Addr) + ":" + str(Port)
+	if(node in neighbors):
+		cost = neighbors[node]
+		routingTable[node] = (cost, node)
+		sendRT(s)
+	else:
+		print("Invalid parameters for LINKDOWN")
+
+def getCommand(s):
+	''' Get commands from user '''
 	while(True):
 		print (">"),
 		commandLine = sys.stdin.readline()
@@ -69,21 +137,40 @@ def getCommand():
 		command_s = commandLine.split(' ')
 
 		command = command_s[0].rstrip()
-
-		if(len(command_s) == 1):
-			parameters = ''
-		
-		else:
-			parameters = command_s[1]
 		
 
 		if (command == "SHOWRT"):
 			for destination in routingTable:
 				print "Destination: " + destination + ", Cost: " + str(routingTable[destination][0]) + ", Link: " + routingTable[destination][1] + "\n",
+		
+		elif (command == "LINKDOWN"):
+			if(len(command_s) == 3):
+				addr  = command_s[1]
+				port = command_s[2].rstrip()
+				linkDown(s, addr, port)
+			else:
+				print("Missing parameters to LINKDOWN")
 
-		if(command == "exit"):
+		elif( command == "LINKUP"):
+			if(len(command_s) == 3):
+				addr  = command_s[1]
+				port = command_s[2].rstrip()
+				linkUp(s, addr, port)
+			else:
+				print("Missing parameters to LINKUP")
+
+		elif(command == "CLOSE"):
 			return
+		
+		else:
+			print ("Invalid command")
 
+def defineDistanceVector(client):
+	for node in routingTable:
+		if(node != client and routingTable[node][1] == client):
+			distanceVector[node] = (float('inf'), "")
+		else:
+			distanceVector[node] = routingTable[node]
 def main(argv):
 
 	distanceVector = {}
@@ -111,7 +198,7 @@ def main(argv):
 	t2 = threading.Thread(target = executeTimeout, args = (timeout, s))
 	t2.daemon = True
 	
-	t3 = threading.Thread(target = getCommand)
+	t3 = threading.Thread(target = getCommand, args = (s, ))
 	t3.daemon = True
 	
 	t3.start()
@@ -125,7 +212,7 @@ def main(argv):
 			if(not t3.is_alive()):
 				return
 		except KeyboardInterrupt:
-			print "\nGoodbye :)"
+			print "\nClosed :)"
 			return
 
 main(sys.argv)
